a31144e9f447dcdad9b255dfd82c4cd5b22bf905
xkcm <maciejka.citko@gmail.com>
2022-02-04 00:59:48 +0100

    Fix minor errors

--- src/app.js
+++ src/app.js
@@ -4,10 +4,13 @@ const parsers = require('./parsers');
 async function classifyText(action, settings) {
     const params = {
         text: parsers.string(action.params.text),
-        modelId: parsers.string(action.params.model),
+        modelId: parsers.string(action.params.model.id),
         productionModel: parsers.boolean(action.params.productionModel),
         apiKey : parsers.string(action.params.MONKEYLEARN_API_KEY) || settings.MONKEYLEARN_API_KEY,
     };
+
+    if (!params.apiKey) throw new Error("API Key is required")
+
     const classifyResponse = await classifyIt(params);
 
     return classifyResponse.body;
@@ -17,10 +20,13 @@ async function classifyText(action, settings) {
 async function extractText(action, settings) {
     const params = {
         text: parsers.string(action.params.text),
-        modelId: parsers.string(action.params.model),
+        modelId: parsers.string(action.params.model.id),
         productionModel: parsers.boolean(action.params.productionModel),
         apiKey : parsers.string(action.params.MONKEYLEARN_API_KEY) || settings.MONKEYLEARN_API_KEY,
     };
+
+    if (!params.apiKey) throw new Error("API Key is required")
+
     const extractResponse = await extractIt(params);
 
     return extractResponse.body;
